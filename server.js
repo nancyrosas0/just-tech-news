17dec0379e3326b166a61c6878e596e7772ee6b2
Nancy <nancyrosas0@gmail.com>
2021-05-12 20:06:40 -0500

    changed force back to false for table updates with models

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(routes);
 // turn on connection to db and server
 // we had the force as false. By making it true, it the db connection must sync w/ the model definitions
 // this allows the table to be overwritten and created
-sequelize.sync({ force: true }).then(() => {
+// run/check it once to update, then change it back to false so it doesn't keep on dropping the tables and seeds
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
