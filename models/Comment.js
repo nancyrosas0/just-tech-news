11f8994b60ffe1928ad248830c09d4fdc9bc2bb1
Nancy <nancyrosas0@gmail.com>
2021-05-15 11:06:30 -0500

    added comment columns

---
+++ models/Comment.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const { truncate } = require('./User');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        // THESE ARE THE COLUMNS
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        comment_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER, 
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
+
+module.exports = Comment;
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote } = require("../../models");
+const { User, Post, Vote, Comment } = require("../../models");
 const sequelize = require('../../config/connection');
 
 // get all users
@@ -7,15 +7,30 @@ const sequelize = require('../../config/connection');
 router.get('/', (req, res) => {
     console.log('================');
     Post.findAll({
+        order: [['created_at', 'DESC']],
         // Query figuration
-        attributes: ['id', 
+        attributes: [
+        'id', 
         'post_url', 
         'title', 
         'created_at',
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
-        order: [['created_at', 'DESC']],
         include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -84,7 +99,7 @@ router.post('/', (req, res) => {
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
     // custom static method created in models/Post.js
-Post.upvote(req.body {Vote})
+Post.upvote(req.body, {Vote})
 .then(updatedPostData => res.json(updatedPostData))
 .catch(err => {
     console.log(err);
