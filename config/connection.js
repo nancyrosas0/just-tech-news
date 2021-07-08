8ec5f07322101329910ad66d6af783191f268de1
Nancy <nancyrosas0@gmail.com>
2021-07-08 13:37:29 -0500

    added login/logout capability

--- config/connection.js
+++ config/connection.js
@@ -13,15 +13,13 @@ require('dotenv').config();
 // });
 
 //module.exports = sequelize;
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
-let sequelize;
+module.exports = sequelize;
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: 3306
-        });
-}
